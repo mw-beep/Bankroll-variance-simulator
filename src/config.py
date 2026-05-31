c1657aa2dba3239676b0a6e5c327726251f1c84c
mw-beep <mw-beep@users.noreply.github.com>
2026-05-31 19:30:22 +0100

    added default starting values in config.py

---
+++ src/config.py
@@ -0,0 +1,11 @@
+# Starting parameters, default values
+# We assume live (casino) poker, 30 hands/hour
+
+STARTING_BANKROLL = 4_000
+HOURLY_WINRATE = 15
+HOURLY_STD_DEV = 120
+HOURS_PER_MONTH = 100
+MONTHS = 12
+NUM_SIMULATIONS = 10_000
+RUIN_THRESHOLD = 0
+TARGET_BANKROLL = 8_000
